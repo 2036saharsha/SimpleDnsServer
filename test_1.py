288159e27441f2f3c4cb9f87639d74e7b31e88de
2036Saharsha <2036saharsha@gmail.com>
2022-05-26 09:41:55 +0545

    Initial

--- test_1.py
+++
@@ -1,7 +0,0 @@
-import socket
-import tldextract
-domainName = input('Enter the domain name: ')
-print(tldextract.extract(domainName))
-domainName=(tldextract.extract(domainName).domain)+"."+(tldextract.extract(domainName).suffix)
-print(domainName)
-print(socket.gethostbyname('www.youtube.com'))
